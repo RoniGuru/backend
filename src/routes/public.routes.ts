342c1900379fedc0d2045f6de8785e98d9ed388a
Ronit Gurung <lecrepes45@gmail.com>
2025-07-17 21:41:12 +0100

    inject auth

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { AuthController } from '../controllers/auth.controller';
+import { AuthService } from '../services/auth.service';
+import { UserRepository } from '../repository/user.respository';
 
-const authController = new AuthController();
+const userRepo = new UserRepository();
+const authService = new AuthService(userRepo);
+const authController = new AuthController(authService);
 const router = express.Router();
 
 router.post('/login', authController.login);
